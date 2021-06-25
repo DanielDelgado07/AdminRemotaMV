2c146dbf3bf2405096da2d4d1d40115fdc2bd506
Daniel Delgado <ddelgado@sumset.com>
2021-06-25 18:54:03 -0500

    creaciónpara el enrutamiento parte inicial

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SharedRoutingModule } from './shared-routing.module';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+
+@NgModule({
+  declarations: [
+    NavbarComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedRoutingModule
+  ]
+})
+export class SharedModule { }
